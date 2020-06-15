124884c22e7d278cb8e0ca4d4d959d5d719d12fd
ronak rabadiya <rabadiyaronak@gmail.com>
2020-06-15 14:32:39 +0530

    add support for config server in order-ms

--- order-ms/src/main/java/learn/rr/microservice/orderms/OrderMsApplication.java
+++ order-ms/src/main/java/learn/rr/microservice/orderms/OrderMsApplication.java
@@ -2,12 +2,12 @@ package learn.rr.microservice.orderms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @SpringBootApplication
-@EnableCassandraRepositories(basePackages = {"learn.rr.microservice.orderms.repository"})
+@EnableDiscoveryClient
 public class OrderMsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(OrderMsApplication.class, args);
     }
