c0d36ecb557ffb8ec8d8d6bf4cbb4ce2ae8259e9
ronak rabadiya <rabadiyaronak@gmail.com>
2020-06-28 20:23:21 +0530

    commit withoutconfig server discovery and gatway

--- product-ms/src/main/java/learn/rr/microservice/productms/ProductMsApplication.java
+++ product-ms/src/main/java/learn/rr/microservice/productms/ProductMsApplication.java
@@ -2,14 +2,13 @@ package learn.rr.microservice.productms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class ProductMsApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ProductMsApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMsApplication.class, args);
+	}
 
 }
--- product-ms/src/main/java/learn/rr/microservice/productms/controller/ProductController.java
+++ product-ms/src/main/java/learn/rr/microservice/productms/controller/ProductController.java
@@ -1,70 +1,84 @@
 package learn.rr.microservice.productms.controller;
 
+import java.util.List;
+import java.util.UUID;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import learn.rr.microservice.productms.dto.ProductDto;
 import learn.rr.microservice.productms.exception.BusinessException;
 import learn.rr.microservice.productms.service.ProductService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import java.util.List;
-import java.util.UUID;
-
-@Api(tags = "product microservice",value = "Product Controller", description = "Controller for all product related operations")
+@Api(tags = "product microservice", value = "Product Controller", description = "Controller for all product related operations")
 @RestController
+@RequestMapping("/products")
 public class ProductController {
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    private ProductService productService;
-    private UUID id;
-    private ProductDto productDto;
+	private ProductService productService;
+	private UUID id;
+	private ProductDto productDto;
 
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
 
-    @ApiOperation(value = "retrive all products or filter products based on query input")
-    @ApiParam(name = "products", value = "list of product id to be fetched")
-    @GetMapping(headers = "API-VERSION=1")
-    public List<ProductDto> getAllProducts(@RequestParam(name = "products",required = false, defaultValue = "") List<UUID> productIds) {
-        if(productIds.isEmpty()) {
-            return productService.getAllProducts();
-        } else {
-            return productService.getAllProductsByIds(productIds);
-        }
-    }
+	@ApiOperation(value = "retrive all products or filter products based on query input")
+	@ApiParam(name = "products", value = "list of product id to be fetched")
+	@GetMapping(headers = "API-VERSION=1")
+	public List<ProductDto> getAllProducts(
+			@RequestParam(name = "products", required = false, defaultValue = "") List<UUID> productIds) {
+		if (productIds.isEmpty()) {
+			return productService.getAllProducts();
+		} else {
+			return productService.getAllProductsByIds(productIds);
+		}
+	}
 
-    @ApiOperation("retrive product by product id")
-    @GetMapping(value = "/{productid}",headers = "API-VERSION=1")
-    public ProductDto getProductById(@PathVariable("productid") UUID id) throws BusinessException {
-        return productService.getProductByProductId(id);
-    }
+	@ApiOperation("retrive product by product id")
+	@GetMapping(value = "/{productid}", headers = "API-VERSION=1")
+	public ProductDto getProductById(@PathVariable("productid") UUID id) throws BusinessException {
+		return productService.getProductByProductId(id);
+	}
 
-    @ApiOperation("Create product")
-    @PostMapping(headers = "API-VERSION=1")
-    public ResponseEntity<Void> createProduct(@Valid @RequestBody ProductDto productDto) throws BusinessException {
-        productService.createProduct(productDto);
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
+	@ApiOperation("Create product")
+	@PostMapping(headers = "API-VERSION=1")
+	public ResponseEntity<Void> createProduct(@Valid @RequestBody ProductDto productDto)
+			throws BusinessException {
+		productService.createProduct(productDto);
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
 
-    @ApiOperation("Update product by id")
-    @PutMapping(value = "/{productid}",headers = "API-VERSION=1")
-    public ResponseEntity<ProductDto> updateProduct(@PathVariable("productid") @NotNull UUID id, @Valid @RequestBody ProductDto productDto) throws BusinessException {
-        ProductDto product = productService.updateProduct(id, productDto);
-        return new ResponseEntity<>(product, HttpStatus.OK);
-    }
+	@ApiOperation("Update product by id")
+	@PutMapping(value = "/{productid}", headers = "API-VERSION=1")
+	public ResponseEntity<ProductDto> updateProduct(@PathVariable("productid") @NotNull UUID id,
+			@Valid @RequestBody ProductDto productDto) throws BusinessException {
+		ProductDto product = productService.updateProduct(id, productDto);
+		return new ResponseEntity<>(product, HttpStatus.OK);
+	}
 
-    @ApiOperation("Delete product")
-    @DeleteMapping(value = "/{productid}" ,headers = "API-VERSION=1")
-    public ResponseEntity<Void> deleteProduct(@PathVariable("productid") @NotNull UUID id) {
-        productService.deleteProduct(id);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+	@ApiOperation("Delete product")
+	@DeleteMapping(value = "/{productid}", headers = "API-VERSION=1")
+	public ResponseEntity<Void> deleteProduct(@PathVariable("productid") @NotNull UUID id) {
+		productService.deleteProduct(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 }
